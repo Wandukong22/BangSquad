0d9b771b5870e3dda4d81a5c6a40c87bd24399ad
97SUN <kswoo97241124@gmail.com>
2026-02-09 19:39:04 +0900

    Feature/ksw/df tver (#340)
    * BSPlayerController

--- Source/Project_Bang_Squad/Game/Base/BSPlayerController.cpp
+++ Source/Project_Bang_Squad/Game/Base/BSPlayerController.cpp
@@ -1,22 +1,34 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "Project_Bang_Squad/Game/Base/BSPlayerController.h"
-
-#include "BSPlayerState.h"
-#include "Blueprint/UserWidget.h"
+#include "Project_Bang_Squad/Game/Base/BSPlayerState.h"
 #include "Project_Bang_Squad/Core/BSGameInstance.h"
+#include "Blueprint/UserWidget.h"
+
+// [QTE System Includes]
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputMappingContext.h"
+#include "InputAction.h"
+#include "Project_Bang_Squad/UI/Stage/Boss/QTEWidget.h"
+#include "Project_Bang_Squad/Character/StageBoss/Stage2Boss.h"
+
+ABSPlayerController::ABSPlayerController()
+{
+	// 기본값 초기화
+	bIsQTEActive = false;
+	CurrentTapCount = 0;
+	GoalTapCount = 0;
+}
 
 void ABSPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
 
+	// [Network] 로컬 플레이어인 경우 직업 정보를 서버에 동기화
 	if (IsLocalController())
 	{
 		if (UBSGameInstance* GI = GetGameInstance<UBSGameInstance>())
 		{
 			EJobType MyJob = GI->GetPlayerJob();
-
 			if (ABSPlayerState* PS = GetPlayerState<ABSPlayerState>())
 			{
 				PS->Server_SetJob(MyJob);
@@ -28,99 +40,187 @@ void ABSPlayerController::BeginPlay()
 void ABSPlayerController::EndPlay(const EEndPlayReason::Type EndPlayReason)
 {
 	Super::EndPlay(EndPlayReason);
-	
-	// 레벨 이동이나 종료 시 관리하던 위젯들을 모두 제거
-	for (UUserWidget* Widget : ManagedWidgets)
+
+	// 1. 관리 중인 일반 위젯 정리
+	for (TObjectPtr<UUserWidget> Widget : ManagedWidgets)
 	{
-		if (IsValid(Widget))
+		if (Widget && Widget->IsInViewport())
 		{
 			Widget->RemoveFromParent();
 		}
 	}
 	ManagedWidgets.Empty();
 
-	
-}
-
-void ABSPlayerController::ServerSetNickName_Implementation(const FString& NewName)
-{
-	if (ABSPlayerState* PS = GetPlayerState<ABSPlayerState>())
+	// 2. QTE 위젯 정리 (맵 이동 시 잔존 방지)
+	if (QTEWidgetInstance && QTEWidgetInstance->IsInViewport())
 	{
-		PS->SetPlayerName(NewName);
+		QTEWidgetInstance->RemoveFromParent();
+		QTEWidgetInstance = nullptr;
 	}
-}
-
-void ABSPlayerController::RegisterManagedWidget(UUserWidget* InWidget)
-{
-	if (InWidget)
-		ManagedWidgets.Add(InWidget);
 }
 
 void ABSPlayerController::SetupInputComponent()
 {
 	Super::SetupInputComponent();
 
+	// 1. [Enhanced Input] 게임플레이 및 QTE 로직용
+	// UE 5.5 표준: CastChecked 대신 안전한 Cast 사용 권장
+	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(InputComponent))
+	{
+		// QTE 연타 액션 바인딩 (Start Trigger)
+		if (QTE_TapAction)
+		{
+			EnhancedInputComponent->BindAction(QTE_TapAction, ETriggerEvent::Started, this, &ABSPlayerController::OnQTETapInput);
+		}
+	}
+
+	// 2. [Legacy Input] 디버그/치트용 (빠른 개발을 위해 기존 방식 유지)
+	// F1 ~ F9 키 바인딩
 	InputComponent->BindKey(EKeys::F1, IE_Pressed, this, &ABSPlayerController::MoveToStage1Map);
 	InputComponent->BindKey(EKeys::F2, IE_Pressed, this, &ABSPlayerController::MoveToStage1MiniGameMap);
 	InputComponent->BindKey(EKeys::F3, IE_Pressed, this, &ABSPlayerController::MoveToStage1BossMap);
+
 	InputComponent->BindKey(EKeys::F4, IE_Pressed, this, &ABSPlayerController::MoveToStage2Map);
 	InputComponent->BindKey(EKeys::F5, IE_Pressed, this, &ABSPlayerController::MoveToStage2MiniGameMap);
 	InputComponent->BindKey(EKeys::F6, IE_Pressed, this, &ABSPlayerController::MoveToStage2BossMap);
+
 	InputComponent->BindKey(EKeys::F7, IE_Pressed, this, &ABSPlayerController::MoveToStage3Map);
 	InputComponent->BindKey(EKeys::F8, IE_Pressed, this, &ABSPlayerController::MoveToStage3MiniGameMap);
 	InputComponent->BindKey(EKeys::F9, IE_Pressed, this, &ABSPlayerController::MoveToStage3BossMap);
 }
 
-void ABSPlayerController::MoveToStage1Map()
+void ABSPlayerController::RegisterManagedWidget(UUserWidget* InWidget)
 {
-	ServerDebugMoveToMapHandle(EStageIndex::Stage1, EStageSection::Main);
+	if (IsValid(InWidget))
+	{
+		ManagedWidgets.Add(InWidget);
+	}
 }
 
-void ABSPlayerController::MoveToStage1MiniGameMap()
+void ABSPlayerController::ServerSetNickName_Implementation(const FString& NewName)
 {
-	ServerDebugMoveToMapHandle(EStageIndex::Stage1, EStageSection::MiniGame);
+	// [Server] PlayerState에 닉네임 전파 (Replication 담당)
+	if (ABSPlayerState* PS = GetPlayerState<ABSPlayerState>())
+	{
+		PS->SetPlayerName(NewName);
+	}
 }
 
-void ABSPlayerController::MoveToStage1BossMap()
+#pragma region Boss QTE System
+
+void ABSPlayerController::Client_StartQTE_Implementation(AStage2Boss* BossActor, int32 TargetCount, float Duration)
 {
-	ServerDebugMoveToMapHandle(EStageIndex::Stage1, EStageSection::Boss);
+	// 로컬 컨트롤러가 아니면 UI나 입력을 처리할 필요 없음
+	if (!IsLocalController()) return;
+
+	// 1. 상태 초기화
+	CurrentBossRef = BossActor;
+	GoalTapCount = TargetCount;
+	CurrentTapCount = 0;
+	bIsQTEActive = true;
+
+	// 2. 입력 컨텍스트(IMC) 교체 (Priority 100으로 설정하여 이동 키 덮어씀)
+	if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()))
+	{
+		if (QTE_Context)
+		{
+			Subsystem->AddMappingContext(QTE_Context, 100);
+		}
+	}
+
+	// 3. QTE 전용 UI 생성 및 표시
+	if (QTEWidgetClass)
+	{
+		// 기존 위젯 안전 제거
+		if (QTEWidgetInstance)
+		{
+			QTEWidgetInstance->RemoveFromParent();
+			QTEWidgetInstance = nullptr;
+		}
+
+		QTEWidgetInstance = CreateWidget<UQTEWidget>(this, QTEWidgetClass);
+		if (QTEWidgetInstance)
+		{
+			QTEWidgetInstance->AddToViewport(100); // UI 최상단 표시
+			// TODO: QTEWidgetInstance->SetDuration(Duration); // 위젯에 시간 정보 전달 가능
+		}
+	}
 }
 
-void ABSPlayerController::MoveToStage2Map()
+void ABSPlayerController::OnQTETapInput()
 {
-	ServerDebugMoveToMapHandle(EStageIndex::Stage2, EStageSection::Main);
+	if (!bIsQTEActive) return;
+
+	CurrentTapCount++;
+
+	// TODO: 위젯에 현재 연타 횟수 업데이트 (QTEWidgetInstance->UpdateProgress(CurrentTapCount, GoalTapCount))
+
+	// 성공 조건 달성 체크
+	if (CurrentTapCount >= GoalTapCount)
+	{
+		bIsQTEActive = false; // 중복 성공 방지
+
+		// [Server RPC] 보스에게 성공 사실 통보
+		if (CurrentBossRef)
+		{
+			// 주의: 보스 클래스에 ServerRPC_QTEResult 함수가 구현되어 있어야 함
+			CurrentBossRef->ServerRPC_QTEResult(this, true);
+		}
+
+		// 클라이언트 측 정리 (성공 연출은 위젯 내부에서 처리 가능)
+		Client_EndQTE(true);
+	}
 }
 
-void ABSPlayerController::MoveToStage2MiniGameMap()
+void ABSPlayerController::Client_EndQTE_Implementation(bool bSuccess)
 {
-	ServerDebugMoveToMapHandle(EStageIndex::Stage2, EStageSection::MiniGame);
+	bIsQTEActive = false;
+
+	// 1. 입력 컨텍스트 제거 (일반 이동 복구)
+	if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()))
+	{
+		if (QTE_Context)
+		{
+			Subsystem->RemoveMappingContext(QTE_Context);
+		}
+	}
+
+	// 2. UI 제거
+	if (QTEWidgetInstance)
+	{
+		QTEWidgetInstance->RemoveFromParent();
+		QTEWidgetInstance = nullptr;
+	}
+
+	// 참조 해제 (메모리 누수 방지)
+	CurrentBossRef = nullptr;
 }
 
-void ABSPlayerController::MoveToStage2BossMap()
-{
-	ServerDebugMoveToMapHandle(EStageIndex::Stage2, EStageSection::Boss);
-}
+#pragma endregion
 
-void ABSPlayerController::MoveToStage3Map()
-{
-	ServerDebugMoveToMapHandle(EStageIndex::Stage3, EStageSection::Main);
-}
-
-void ABSPlayerController::MoveToStage3MiniGameMap()
-{
-	ServerDebugMoveToMapHandle(EStageIndex::Stage3, EStageSection::MiniGame);
-}
-
-void ABSPlayerController::MoveToStage3BossMap()
-{
-	ServerDebugMoveToMapHandle(EStageIndex::Stage3, EStageSection::Boss);
-}
+#pragma region Debug Moves
 
 void ABSPlayerController::ServerDebugMoveToMapHandle_Implementation(EStageIndex Stage, EStageSection Section)
 {
+	// [Server] GameInstance를 통해 맵 트래블 실행
 	if (UBSGameInstance* GI = GetGameInstance<UBSGameInstance>())
 	{
 		GI->MoveToStage(Stage, Section);
 	}
 }
 
+// --- Wrapper Functions (Input Bindings) ---
+
+void ABSPlayerController::MoveToStage1Map() { ServerDebugMoveToMapHandle(EStageIndex::Stage1, EStageSection::Main); }
+void ABSPlayerController::MoveToStage1MiniGameMap() { ServerDebugMoveToMapHandle(EStageIndex::Stage1, EStageSection::MiniGame); }
+void ABSPlayerController::MoveToStage1BossMap() { ServerDebugMoveToMapHandle(EStageIndex::Stage1, EStageSection::Boss); }
+
+void ABSPlayerController::MoveToStage2Map() { ServerDebugMoveToMapHandle(EStageIndex::Stage2, EStageSection::Main); }
+void ABSPlayerController::MoveToStage2MiniGameMap() { ServerDebugMoveToMapHandle(EStageIndex::Stage2, EStageSection::MiniGame); }
+void ABSPlayerController::MoveToStage2BossMap() { ServerDebugMoveToMapHandle(EStageIndex::Stage2, EStageSection::Boss); }
+
+void ABSPlayerController::MoveToStage3Map() { ServerDebugMoveToMapHandle(EStageIndex::Stage3, EStageSection::Main); }
+void ABSPlayerController::MoveToStage3MiniGameMap() { ServerDebugMoveToMapHandle(EStageIndex::Stage3, EStageSection::MiniGame); }
+void ABSPlayerController::MoveToStage3BossMap() { ServerDebugMoveToMapHandle(EStageIndex::Stage3, EStageSection::Boss); }
+
+#pragma endregion
