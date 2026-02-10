09b59610884bb26567b5037c7bcd71b18965924f
Wandukong22 <dpdnjs512@naver.com>
2026-02-10 13:53:42 +0900

    MiniGame

--- Source/Project_Bang_Squad/Game/MiniGame/MiniGameMode.h
+++ Source/Project_Bang_Squad/Game/MiniGame/MiniGameMode.h
@@ -18,25 +18,13 @@ class PROJECT_BANG_SQUAD_API AMiniGameMode : public ABSGameMode
 public:
 	AMiniGameMode();
 
-	//접속 시 스폰 처리
-	virtual void PostLogin(APlayerController* NewPlayer) override;
-
-	//실제 소환 함수
-	void SpawnPlayerCharacter(AController* Controller, EJobType JobType);
-
-	//사망 처리
-	void RequestRespawn(AController* DeadPlayer);
-
 	//도착 처리
 	void OnPlayerReachedGoal(AController* ReachedPlayer);
 	
 
 protected:
-	//부활 함수(5초 뒤)
-	void ExecuteRespawn(AController* Controller);
-
 	//부활 위치 계산
-	FTransform GetRespawnTransform(AController* Controller);
+	virtual FTransform GetRespawnTransform(AController* Controller) override;
 
 	//도착한 플레이어 목록
 	UPROPERTY()
@@ -44,7 +32,4 @@ protected:
 
 	//모두 도착했는지 체크
 	void CheckAllPlayersFinished();
-
-	//UPROPERTY(EditAnywhere, Category = "BS|MiniGame")
-	//float RespawnTime = 5.f;
 };
