8f52e9f9251598e7d6434a2bd7abbda7bbb5604a
Wandukong22 <dpdnjs512@naver.com>
2026-02-23 14:49:54 +0900

    쓸데없는 코드 제거

--- Source/Project_Bang_Squad/Game/MiniGame/ArenaMiniGameMode.cpp
+++ Source/Project_Bang_Squad/Game/MiniGame/ArenaMiniGameMode.cpp
@@ -31,32 +31,6 @@ void AArenaMiniGameMode::OnPlayerDied(AController* VictimController)
 		if (AlivePlayerCount <= 1)
 		{
 			EndArena();
-			/*GetWorldTimerManager().ClearTimer(ArenaTimerHandle);
-
-			for (FConstPlayerControllerIterator It = GetWorld()->GetPlayerControllerIterator(); It; ++It)
-			{
-				APlayerController* PC = Cast<APlayerController>(It->Get());
-				if (PC)
-				{
-					AArenaPlayerState* ArenaPS = Cast<AArenaPlayerState>(PC->PlayerState);
-					if (ArenaPS && ArenaPS->GetIsAlive() && PC->GetPawn())
-					{
-						PC->GetPawn()->SetCanBeDamaged(false);
-						//TODO: UI 띄우기
-						break;
-					}
-				}
-			}
-
-			//Stage3 Main으로 이동
-			FTimerHandle ReturnTimerHandle;
-			GetWorldTimerManager().SetTimer(ReturnTimerHandle, FTimerDelegate::CreateLambda([this]()
-			{
-				if (UBSGameInstance* GI = Cast<UBSGameInstance>(GetGameInstance()))
-				{
-					GI->MoveToStage(EStageIndex::Stage3, EStageSection::Main);
-				}
-			}), 5.f, false);*/
 		}
 	}
 }
@@ -221,7 +195,7 @@ void AArenaMiniGameMode::EndArena()
 		}
 	}
 
-	// N초 후 Stage3 Main으로 이동
+	// 5초 후 Stage3 Main으로 이동
 	FTimerHandle ReturnHandle;
 	GetWorldTimerManager().SetTimer(ReturnHandle, FTimerDelegate::CreateLambda([this]()
 	{
@@ -231,54 +205,3 @@ void AArenaMiniGameMode::EndArena()
 		}
 	}), 5.f, false);
 }
-
-/*void AArenaMiniGameMode::UpdateArenaTimer()
-{
-	AArenaGameState* GS = GetGameState<AArenaGameState>();
-	if (!GS) return;
-
-	GS->SetRemainingTime(GS->GetRemainingTime() - 1);
-
-	if (GS->GetRemainingTime() <= 0)
-	{
-		EArenaPattern CurrentPhase = GS->GetCurrentPhase();
-		//페이즈 전환
-		if (CurrentPhase == EArenaPattern::None)
-		{
-			GS->SetCurrentPhase(EArenaPattern::Surviving);
-			GS->SetRemainingTime(20);
-		}
-		else if (CurrentPhase == EArenaPattern::Surviving)
-		{
-			GS->SetCurrentPhase(EArenaPattern::FloorSinking);
-			GS->SetRemainingTime(5);
-			//TODO: 바닥 가라앉는 이벤트?
-			int32 TargetFloorNum = GS->GetCurrentSinkingFloor();
-
-			/*
-			for (TActorIterator<AArenaFloor> It(GetWorld()); It; ++It)
-			{
-				AArenaFloor* Floor = *It;
-				if (Floor && Floor->FloorNumber == TargetFloorNum)
-				{
-					// 멀티캐스트 RPC나 RepNotify를 통해 클라이언트 측에서도 가라앉는 연출 실행
-					Floor->Multicast_StartSinking(); 
-				}
-			}
-			#1#
-		}
-		else if (CurrentPhase == EArenaPattern::FloorSinking)
-		{
-			GS->SetCurrentSinkingFloor(GS->GetCurrentSinkingFloor() + 1);
-			if (GS->GetCurrentSinkingFloor() <= 2)
-			{
-				GS->SetCurrentPhase(EArenaPattern::Surviving);
-				GS->SetRemainingTime(20);
-			}
-			else
-			{
-				GetWorldTimerManager().ClearTimer(ArenaTimerHandle);
-			}
-		}
-	}
-}*/
