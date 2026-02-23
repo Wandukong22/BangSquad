6b875a6fdd4379300ab048d20ec1397b7b38006a
Jangsaehyeon <s1206h@naver.com>
2026-02-23 11:29:56 +0900

    Add Boss3 elevator actor and assets
    Introduce ABoss3Elevator (CPP/H) which moves an elevator along Z using a UTimeline and UCurveFloat (RiseCurve) with configurable RiseHeight and linked TargetMachines. The actor exposes ActivateElevator to start the timeline, interpolates actor location between StartLocation and EndLocation, and uses reflection to call "Activate" and "Deactivate" on connected blueprint machines. Also adds BP_Boss3Elevator.uasset, Curve_Accel.uasset, Curve_Decel.uasset, and a new Stage3_Boss.umap; updates Stage3_Demo.umap and modifies RotateMachine.uasset.

---
+++ Source/Project_Bang_Squad/MapPuzzle/Boss3Elevator.h
@@ -0,0 +1,58 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Components/TimelineComponent.h"
+#include "Boss3Elevator.generated.h"
+
+class UStaticMeshComponent;
+class UCurveFloat;
+
+UCLASS()
+class PROJECT_BANG_SQUAD_API ABoss3Elevator : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ABoss3Elevator();
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+
+public:
+	// 스위치 등 외부에서 이 함수를 호출하여 엘리베이터 가동
+	UFUNCTION(BlueprintCallable, Category = "Boss3")
+	void ActivateElevator();
+
+protected:
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	TObjectPtr<UStaticMeshComponent> MeshComp;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	TObjectPtr<UTimelineComponent> ElevatorTimeline;
+
+	UPROPERTY(EditAnywhere, Category = "Elevator Settings")
+	TObjectPtr<UCurveFloat> RiseCurve;
+
+	UPROPERTY(EditAnywhere, Category = "Elevator Settings")
+	float RiseHeight = 1000.0f;
+
+	UPROPERTY(VisibleInstanceOnly, Category = "Elevator Settings")
+	bool bIsActivated = false;
+
+
+	UPROPERTY(EditInstanceOnly, Category = "Linked Systems")
+	TArray<AActor*> TargetMachines;
+
+
+	UFUNCTION()
+	void HandleTimelineProgress(float Value);
+
+	UFUNCTION()
+	void OnTimelineFinished();
+
+private:
+	FVector StartLocation;
+	FVector EndLocation;
+};
